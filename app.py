d8f89bdbc4d4515166d77a87e5558c0f37d63cb0
simplecod3r <digofern@unifei.edu.br>
2021-12-19 23:59:43 -0300

    coment useless code

--- app.py
+++ app.py
@@ -11,19 +11,7 @@ def index():
     #games = gameList()
     #print(games)
     return render_template("index.html")
-    
-@app.route("/ps4")
-def ps4():
-    return render_template("ps4.html")
-    
-@app.route("/xbox")
-def xbox():
-    return render_template("xbox.html")
-    
-@app.route("/switch")
-def switch():
-    return render_template("switch.html")
-    
+        
 @app.route("/steam")
 def steam():
     return render_template("steam.html")
